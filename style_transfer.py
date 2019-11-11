3f310c713ea0f486f640b51e4af1245f67edc29b
jralha <42118093+jralha@users.noreply.github.com>
2019-11-11 17:24:02 -0300

    correct indentation

--- style_transfer.py
+++ style_transfer.py
@@ -51,7 +51,7 @@ num_style_layers = len(style_layers)
 #####################################################################
 def vgg_layers(layer_names):
     """ Creates a vgg model that returns a list of intermediate output values."""
-  # Load our model. Load pretrained VGG, trained on imagenet data
+    # Load our model. Load pretrained VGG, trained on imagenet data
     vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
     vgg.trainable = False
   
@@ -71,29 +71,29 @@ def gram_matrix(input_tensor):
 
 class StyleContentModel(tf.keras.models.Model):
     def __init__(self, style_layers, content_layers):
-      super(StyleContentModel, self).__init__()
-      self.vgg =  vgg_layers(style_layers + content_layers)
-      self.style_layers = style_layers
-      self.content_layers = content_layers
-      self.num_style_layers = len(style_layers)
-      self.vgg.trainable = False
+        super(StyleContentModel, self).__init__()
+        self.vgg =  vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
 
     def call(self, inputs):
-      "Expects float input in [0,1]"
-      inputs = inputs*255.0
-      preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
-      outputs = self.vgg(preprocessed_input)
-      style_outputs, content_outputs = (outputs[:self.num_style_layers], 
+        "Expects float input in [0,1]"
+        inputs = inputs*255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+        style_outputs, content_outputs = (outputs[:self.num_style_layers], 
                                       outputs[self.num_style_layers:])
 
-      style_outputs = [gram_matrix(style_output)
+        style_outputs = [gram_matrix(style_output)
                       for style_output in style_outputs]
 
-      content_dict = {content_name:value 
+        content_dict = {content_name:value 
                       for content_name, value 
                       in zip(self.content_layers, content_outputs)}
 
-      style_dict = {style_name:value
+        style_dict = {style_name:value
                     for style_name, value
                     in zip(self.style_layers, style_outputs)}
     
@@ -139,9 +139,9 @@ total_variation_weight=10
 @tf.function()
 def train_step(image):
     with tf.GradientTape() as tape:
-      outputs = extractor(image)
-      loss = style_content_loss(outputs)
-      loss += total_variation_weight*tf.image.total_variation(image)
+        outputs = extractor(image)
+        loss = style_content_loss(outputs)
+        loss += total_variation_weight*tf.image.total_variation(image)
 
     grad = tape.gradient(loss, image)
     opt.apply_gradients([(grad, image)])
@@ -152,16 +152,16 @@ image = tf.Variable(content_image)
 import time
 start = time.time()
 
-epochs = 10
+epochs = 100
 steps_per_epoch = 100
 
 step = 0
 for n in range(epochs):
     print("Train epoch: {}".format(n))
     for m in tqdm(range(steps_per_epoch),position=0):
-      step += 1
-      train_step(image)
-      #print(".", end='')
+        step += 1
+        train_step(image)
+        #print(".", end='')
   
 
 end = time.time()
